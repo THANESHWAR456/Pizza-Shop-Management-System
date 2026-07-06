97ed7b294b2e66cc9668e861a655360a13756c19
THANESHWAR456 <thaneshwar12143@gmail.com>
2026-07-06 09:19:28 +0530

    Add pizza shop menu

---
+++ src/PizzaShop.java
@@ -0,0 +1,13 @@
+public class PizzaShop {
+    public static void main(String [] args) {
+        System.out.println("=================================");
+System.out.println("     THANESH PIZZA SHOP");
+System.out.println("=================================");
+System.out.println("1. Veg Pizza        200");
+System.out.println("2. Chicken Pizza    350");
+System.out.println("3. Cheese Pizza     250");
+System.out.println("4. Coke             50");
+System.out.println("5. Exit");
+System.out.println("=================================");
+    }
+}
